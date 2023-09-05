41b20a11c0616cf739b23d49177fc0369479f36c
Pedro Lucas <izielucas2016@gmail.com>
2023-09-05 12:45:08 -0300

    Express começado

---
+++ 03_express/01_rotas.js
@@ -0,0 +1,22 @@
+const express = require("express") //esse codigo irá importar o express, ele retorna uma funcao que cria o express
+const app = express() //a variavel app esta recebendo a funcao que vem do modulo express, que esta funcao cria uma copia inteira do framework para toda a variavel app, ela é a principal do sistema, é recomendado usar ela como const para não perder dados
+
+//Para abrir um servido no express, apenas chamamos a variavel do express: app; depois a função listen, e dentro dela a sua porta:
+//app.listen(8081)
+//Porem, está função sempre tem que ser a ultima!!!
+
+//Ao executar o servidor, percebe-se escrito: "Cannot GET /", isso se da por que a aplicação ainda não tem rota definida
+
+//Oque são rotas? É o caminho para a aplicação
+
+//Como crio ela?
+
+app.get("/", function(req, res){
+    res.send("Hello World, Seja bem vindo!!!") //Resposta do servidor
+})
+
+//Para executar callback voce coloca a function apos a virgula e coloca o callback no console
+//localhost:8081
+app.listen(8081, function(){ //app.listen é executada e fala pro node que esta rodando, logo o node executa uma função em seguida
+    console.log("Servidor rodando!!!")
+})
